fa8bac62ea1e4eac202e4c62895e8696234faa47
Jefferson_Lima_Silva <jefferson.silva@forlogic.net>
2025-05-30 16:36:11 -0300

    #96116 Pagina Dashboard quase completa

--- script.js
+++ script.js
@@ -30,16 +30,26 @@ function showCadPend(){
 
 function criaLista(){
     let list= document.getElementById('box-list')
+    let divlist=document.getElementById('list-div')
     let spanNome= document.createElement('span')
     spanNome.textContent="Jefferson"
     let spanEmail = document.createElement('span')
     spanEmail.textContent= "jefferson@gamil.com"
+    spanEmail.style.marginLeft='25px'
     let spanStatus = document.createElement('span')
     spanStatus.textContent ="Ativo"
+    spanStatus.style.textAlign= 'end'
+    spanStatus.style.marginRight='40px'
+    let hrlist = document.createElement('hr')
+    hrlist.classList.add('list-line')
+    hrlist.style.width='980px'
+    hrlist.style.height='0.1px'
+    hrlist.style.marginLeft='10px'
 
     list.appendChild(spanNome)
     list.appendChild(spanEmail)
     list.appendChild(spanStatus)
+    divlist.appendChild(hrlist)
 }
 
 
