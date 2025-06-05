b19c83964009e98d1cf9a00c9d4d8cacfd92f957
Jefferson_Lima_Silva <jefferson.silva@forlogic.net>
2025-06-05 17:25:37 -0300

    #96676

---
+++ js/mudatema.js
@@ -0,0 +1,6 @@
+document.getElementById('muda-tema').addEventListener('click', ()=>{
+   let colorAll= document.querySelectorAll('body')
+   colorAll.style.color='red'
+   console.log(colorAll);
+   
+})
