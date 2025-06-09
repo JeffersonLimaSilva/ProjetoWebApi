a8b62a70250c6d15a1d6ae4626e5f5eddcd7ea5c
Jefferson_Lima_Silva <jefferson.silva@forlogic.net>
2025-06-09 14:44:05 -0300

    #96677

--- js/listalogs.js
+++ js/listalogs.js
@@ -3,39 +3,44 @@ function criaListaLogs( search =''){
     let userOn = JSON.parse(localStorage.getItem('userOn')) || []
     let logsuser = JSON.parse(localStorage.getItem('logsuser')) || []
 
+    let tbody = document.getElementById('tbody-logs')
+    tbody.innerHTML=''
     
     
-
     logsuser[userOn.index].forEach(lista => {
 
-        console.log(lista);
+        let minName = lista.name.toLowerCase()
+        let minEmail = lista.email.toLowerCase()
+        let minInfor = lista.infor.toLowerCase()
 
-        let tbody = document.getElementById('tbody-logs')
+        if(lista.data.includes(search) || minName.includes(search) || minEmail.includes(search) || minInfor.includes(search)){
+            
+            let trbody = document.createElement('tr')
+            let tddate = document.createElement('td')
+            let tdname = document.createElement('td')
+            let tdemail = document.createElement('td')
+            let tdaction = document.createElement('td')
 
-        let trbody = document.createElement('tr')
-        let tddate = document.createElement('td')
-        let tdname = document.createElement('td')
-        let tdemail = document.createElement('td')
-        let tdaction = document.createElement('td')
+            tddate.textContent =   lista.data
+            tdname.textContent =   lista.name
+            tdemail.textContent =  lista.email
+            tdaction.textContent = lista.infor
 
-        tddate.textContent = lista.data
-        tdname.textContent = lista.name
-        tdemail.textContent = lista.email
-        tdaction.textContent = lista.infor
+            trbody.appendChild(tddate)
+            trbody.appendChild(tdname)
+            trbody.appendChild(tdemail)
+            trbody.appendChild(tdaction)
 
-        trbody.appendChild(tddate)
-        trbody.appendChild(tdname)
-        trbody.appendChild(tdemail)
-        trbody.appendChild(tdaction)
-
-        tbody.appendChild(trbody)
-
-    
-        
+            tbody.appendChild(trbody)
+        }
         
     });
 
 
 }
-
+document.getElementById('search').addEventListener('input', function(e){
+    let minSearch = this.value.toLowerCase()
+    criaListaLogs(minSearch)
+    
+})
 criaListaLogs()
---
+++ js/restricaologin.js
@@ -0,0 +1,12 @@
+function verificaLogin(){
+    let userOn = JSON.parse(localStorage.getItem('userOn'))
+
+
+    if(!userOn.email == '' || !userOn.index == ''){
+
+        window.location.href = '/index.html';
+    }
+    
+}
+
+verificaLogin()
